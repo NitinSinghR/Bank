8c7d4e5d98c0a2d4d21febbd0e2699d9632f8c7c
NitinSinghR <nitin.singh@tringapps.com>
2023-02-28 09:56:31 +0530

    Add files via upload

--- src/main/java/org/example/BankAcc.java
+++ src/main/java/org/example/BankAcc.java
@@ -1,9 +1,7 @@
 package org.example;
 
 import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
+import java.util.logging.*;
 
 class BankAcc {
     public static void main(String[] args) {
@@ -32,24 +30,21 @@ class BankAcc {
             l.info("1.Deposit \n 2.Withdraw \n 3.Balance\n");
             int ch = sc.nextInt();
             switch (ch) {
-                case 1: {
+                case 1 -> {
                     l.info("Enter the Amount:\n");
                     amount = sc.nextInt();
                     b1.deposit(amount);
-                    break;
                 }
-                case 2: {
+                case 2 -> {
                     l.info("Enter the Amount:\n");
                     amount = sc.nextInt();
                     b1.withdraw(amount);
-                    break;
                 }
-                case 3: {
-                    String e=String.valueOf(b1.getBalance());
+                case 3 -> {
+                    String e = String.valueOf(b1.getBalance());
                     l.info(e);
-                    break;
                 }
-                default: {
+                default -> {
                     l.info("Input error!!");
                     sc.close();
                     System.exit(0);
@@ -58,39 +53,3 @@ class BankAcc {
         }
     }
 }
-class Bank extends BankAcc {
-    private String name;
-    private int accNumber;
-    private double balance;
-    Logger l=Logger.getLogger("com.api.jar");
-
-    public Bank(String userName, int accNumber, double balance) {
-        this.name = userName;
-        this.accNumber = accNumber;
-        this.balance = balance;
-    }
-
-    public void deposit(int amount) {
-        balance += amount;
-        String g="Hello  "+name+"\nYour current balance for account  "+accNumber+ "  is  "+balance;
-        l.info(g);
-    }
-
-    public void withdraw(int amount) {
-        if (amount > balance) {
-            l.info("Insufficient Balance for withdraw!!\n");
-        }
-        else {
-                balance -= amount;
-                String f="Hello  " + name + "\nYour current balance for account  " + accNumber + "  is  " + balance;
-                l.info(f);
-        }
-    }
-
-
-    public double getBalance() {
-
-        return balance;
-    }
-}
-
